4f944692512eab6806f26b8cbefd652ce341548f
Asya <web.zaslavskaya@gmail.com>
2024-02-27 16:06:53 +0200

    arrows and pics

--- js/main.js
+++ js/main.js
@@ -92,8 +92,13 @@ countDownTimer();
   $('.main-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:false,
+    nav:true,
     autoplay: true,
+    navText: [
+        '<i class="bi bi-chevron-left"></i>',
+        '<i class="bi bi-chevron-right"></i>',
+      ],
+
     responsive:{
         0:{
             items:1
