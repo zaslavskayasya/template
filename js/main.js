4b64cdf268216621b362c6617d73c09b291341fc
Asya <web.zaslavskaya@gmail.com>
2024-02-28 15:27:21 +0200

    add slider mobile

--- js/main.js
+++ js/main.js
@@ -111,7 +111,28 @@ countDownTimer();
         }
     }
 })
+$('.main-carousel-mob').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    navText: [
+        '<i class="bi bi-chevron-left"></i>',
+        '<i class="bi bi-chevron-right"></i>',
+      ],
 
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 
   // Testimonials carousel
   $(".testimonial-carousel").owlCarousel({
